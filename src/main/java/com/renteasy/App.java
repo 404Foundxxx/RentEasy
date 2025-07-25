fdd1f630d591cd0ecdf085504a11fe5e572cbbad
404Foundxxx <gmarte303@gmail.com>
2025-07-24 21:02:29 -0400

    Create App.java

---
+++ src/main/java/com/renteasy/App.java
@@ -0,0 +1,15 @@
+package com.renteasy;
+
+import com.renteasy.views.*;
+
+/**
+ *
+ * @author gmart
+ */
+public class App {
+
+    public static void main(String[] args) {
+        LoginGUI lgui = new LoginGUI();
+        lgui.setVisible(true);
+    }
+}

e4ad7aea0dceca003551fecea6138e9ba83d88e2
404Foundxxx <gmarte303@gmail.com>
2025-07-24 21:02:32 -0400

    Create ConexionBD.java

---
+++ src/main/java/com/renteasy/database/ConexionBD.java
@@ -0,0 +1,62 @@
+package com.renteasy.database;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author gmart
+ */
+// Clase para manejar la conexión a la base de datos usando el patrón Singleton
+class ConexionBD {
+
+    private static Connection conexion = null;
+
+    public static Connection obtenerConexion() throws FileNotFoundException, SQLException {
+        if (conexion != null && !conexion.isClosed()) {
+            return conexion;
+        } else {
+            try {
+                // Obtener la ruta del directorio actual
+                String directorioActual = System.getProperty("user.dir");
+
+                // Construir la ruta completa del archivo
+                String rutaArchivo = directorioActual + File.separator + "src\\main\\java\\com\\renteasy\\utils\\configuracion.txt";
+                // Abrir el archivo
+                File archivo = new File(rutaArchivo);
+                FileReader lector = new FileReader(archivo);
+                String url;
+                String usuario;
+                String contraseña;
+
+                // Leer el contenido del archivo y almacenarlo en variables
+                BufferedReader buffer;
+                buffer = new BufferedReader(lector);
+                // Leer el contenido del archivo y almacenarlo en variables
+                url = buffer.readLine();
+                usuario = buffer.readLine();
+                contraseña = buffer.readLine();
+                // Cerrar el buffer de lectura
+                buffer.close();
+                conexion = DriverManager.getConnection(url, usuario, contraseña);
+                return conexion;
+
+            } catch (IOException e) {
+                System.out.println("Mensaje (conexion):" + e.getMessage());
+                return null;
+            }
+        }
+    }
+
+    public static void cerrarConexion() throws SQLException {
+        if (conexion != null && !conexion.isClosed()) {
+            conexion.close();
+        }
+    }
+}

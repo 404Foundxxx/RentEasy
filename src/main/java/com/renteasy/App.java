a23559f17628dbdedddf7a2e463a86c473954482
LuisDavidMV12 <luisdavidmoreta12@gmail.com>
2025-07-28 18:59:23 -0400

    Agregue un hola

--- src/main/java/com/renteasy/App.java
+++ src/main/java/com/renteasy/App.java
@@ -11,5 +11,7 @@ public class App {
     public static void main(String[] args) {
         LoginGUI lgui = new LoginGUI();
         lgui.setVisible(true);
+        
+        System.out.println("hola");
     }
 }

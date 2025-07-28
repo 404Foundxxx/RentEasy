7418e82c3925b021eacdacf8480fb35c6a8bc52d
404Foundxxx <gmarte303@gmail.com>
2025-07-28 19:01:02 -0400

    Elimine el hola

--- src/main/java/com/renteasy/App.java
+++ src/main/java/com/renteasy/App.java
@@ -11,7 +11,5 @@ public class App {
     public static void main(String[] args) {
         LoginGUI lgui = new LoginGUI();
         lgui.setVisible(true);
-        
-        System.out.println("hola");
     }
 }

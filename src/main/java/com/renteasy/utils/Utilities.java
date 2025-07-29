f71776c5c1b6fbfabffc6e2d2668e9f6d03502df
404Foundxxx <gmarte303@gmail.com>
2025-07-29 12:55:30 -0400

    Create Utilities.java
    Clase Utilities para utilidades necesitadas

---
+++ src/main/java/com/renteasy/utils/Utilities.java
@@ -0,0 +1,32 @@
+package com.renteasy.utils;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author gmart
+ */
+public class Utilities {
+
+    public static void FlatLaf() {
+        FlatLightLaf.setup();
+        UIManager.put("PasswordField.showRevealButton", true);
+    }
+
+    public static void setIconoVentana(JFrame frame) {
+        URL location = Utilities.class.getResource("/images/EasyPequeno.png");
+        if (location != null) {
+            ImageIcon icon = new ImageIcon(location);
+            frame.setIconImage(icon.getImage());
+        } else {
+            System.err.println("No se encontró el ícono en /images/EasyPequeno.png");
+        }
+    }
+
+}
+
+

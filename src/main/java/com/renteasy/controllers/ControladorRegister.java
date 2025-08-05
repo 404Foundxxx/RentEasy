96f8a952cad3f4eed26b2caba306ca1ac7f25130
404Foundxxx <gmarte303@gmail.com>
2025-08-05 11:36:19 -0400

    Clases controladoras para los frame FrmLogin y FrmRegister

---
+++ src/main/java/com/renteasy/controllers/ControladorRegister.java
@@ -0,0 +1,16 @@
+package com.renteasy.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author gmart
+ */
+public class ControladorRegister implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+}

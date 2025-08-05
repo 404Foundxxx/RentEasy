dafcc25f3c6bb8eef884667a820b199d3a3267ff
404Foundxxx <gmarte303@gmail.com>
2025-08-05 11:44:00 -0400

    Ahora se centra el frame

--- src/main/java/com/renteasy/views/FrmRegister.java
+++ src/main/java/com/renteasy/views/FrmRegister.java
@@ -106,6 +106,7 @@ public class FrmRegister extends javax.swing.JFrame {
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1080, -1));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jLabel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseClicked

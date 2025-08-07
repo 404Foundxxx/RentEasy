92a6555ecd85989cc3317be6efeccf73243d5a16
404Foundxxx <gmarte303@gmail.com>
2025-08-07 00:25:30 -0400

    Create ControladorPublicarPropiedad.java

---
+++ src/main/java/com/renteasy/controllers/ControladorPublicarPropiedad.java
@@ -0,0 +1,21 @@
+package com.renteasy.controllers;
+
+import com.renteasy.views.FrmPublicarPropiedad;
+
+/**
+ *
+ * @author gmart
+ */
+public class ControladorPublicarPropiedad {
+
+    FrmPublicarPropiedad frmPublicarPropiedad = new FrmPublicarPropiedad();
+
+    public ControladorPublicarPropiedad() {
+
+    }
+
+    public ControladorPublicarPropiedad(FrmPublicarPropiedad fpp) {
+        this.frmPublicarPropiedad = fpp;
+    }
+
+}

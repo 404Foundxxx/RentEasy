0378f0d45c3debe947c52624b70b3a95ba20773b
404Foundxxx <gmarte303@gmail.com>
2025-08-07 00:49:49 -0400

    Update ControladorPublicarPropiedad.java

--- src/main/java/com/renteasy/controllers/ControladorPublicarPropiedad.java
+++ src/main/java/com/renteasy/controllers/ControladorPublicarPropiedad.java
@@ -1,6 +1,11 @@
 package com.renteasy.controllers;
 
+import com.renteasy.views.FrmContacto;
+import com.renteasy.views.FrmInicio;
+import com.renteasy.views.FrmLogin;
 import com.renteasy.views.FrmPublicarPropiedad;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  *
@@ -8,7 +13,10 @@ import com.renteasy.views.FrmPublicarPropiedad;
  */
 public class ControladorPublicarPropiedad {
 
-    FrmPublicarPropiedad frmPublicarPropiedad = new FrmPublicarPropiedad();
+    private FrmPublicarPropiedad frmPublicarPropiedad = new FrmPublicarPropiedad();
+    private FrmInicio frmInicio = new FrmInicio();
+    private FrmContacto frmContacto = new FrmContacto();
+    private FrmLogin frmLogin = new FrmLogin();
 
     public ControladorPublicarPropiedad() {
 
@@ -16,6 +24,36 @@ public class ControladorPublicarPropiedad {
 
     public ControladorPublicarPropiedad(FrmPublicarPropiedad fpp) {
         this.frmPublicarPropiedad = fpp;
+
+        // Label Inicio
+        this.frmPublicarPropiedad.lblInicio.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new ControladorInicio(frmInicio);
+                frmInicio.setVisible(true);
+                frmPublicarPropiedad.dispose();
+            }
+        });
+        // Label Contacto
+        this.frmPublicarPropiedad.lblContacto.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new ControladorContacto(frmContacto);
+                frmContacto.setVisible(true);
+                frmPublicarPropiedad.dispose();
+
+            }
+        });
+        // Label Cerrar Sesion
+        this.frmPublicarPropiedad.lblCerrarSesion.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new ControladorLogin(frmLogin);
+                frmLogin.setVisible(true);
+                frmPublicarPropiedad.dispose();
+
+            }
+        });
     }
 
 }
